7cb203479ae10f9a8253d74625259c993bbd7774
necatiozmen <necatiozmn@gmail.com>
2021-02-07 18:11:13 +0300

    Add eslint & prettier

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossOrigin="anonymous"
-        />
-      </Head>
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    return (
+        <>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                    crossOrigin="anonymous"
+                />
+            </Head>
 
-      <Component {...pageProps} />
-    </>
-  );
+            <Component {...pageProps} />
+        </>
+    );
 }
