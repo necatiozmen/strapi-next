d7c71274343356b4bb95d98ca5c4bd1a506b3da0
necatiozmen <necatiozmn@gmail.com>
2021-02-07 17:17:37 +0300

    Add bootstrap & ts

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
